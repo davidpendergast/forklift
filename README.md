4e78dcda037c06118ea2db2a018888f1951df3cd
davidpendergast <dlp75@case.edu>
2022-12-27 04:29:21 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Untitled Forklift Game
+A puzzle game about forklifts.
+
+Here are the five most recent gifs committed:
+
+![007-scenery.gif](gifs/007-scenery.gif?raw=true "007-scenery")
+
+![006-forklift-in-pygame.gif](gifs/006-forklift-in-pygame.gif?raw=true "006-forklift-in-pygame")
+
+![005-forklift-3d.gif](gifs/005-forklift-3d.gif?raw=true "005-forklift-3d")
+
+![004-boxes.gif](gifs/004-boxes.gif?raw=true "004-boxes")
+
+![003-planks.gif](gifs/003-planks.gif?raw=true "003-planks")
--- src/game/forklift.py
+++ src/game/forklift.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 import src.engine.game as game
 import src.engine.threedee as threedee
@@ -6,6 +7,9 @@ import src.engine.globaltimer as globaltimer
 import src.engine.scenes as scenes
 import src.engine.sprites as sprites
 import src.engine.layers as layers
+import src.engine.readme_writer as readme_writer
+import src.utils.util as util
+import configs
 
 import src.game.spriteref as spriteref
 import src.game.menus as menus
@@ -21,6 +25,9 @@ class ForkliftGame(game.Game):
         yield layers.ImageLayer(spriteref.LAYER_DEBUG, 1000)
 
     def initialize(self):
+        if configs.is_dev:
+            _update_readme()
+
         globaltimer.set_show_fps(True)
         scenes.create_instance(menus.Test3DMenu())
 
@@ -30,3 +37,25 @@ class ForkliftGame(game.Game):
 
     def all_sprites(self):
         return scenes.get_instance().all_sprites()
+
+
+def _update_readme():
+    gif_directory = "gifs"
+    gif_filenames = [f for f in os.listdir(gif_directory) if os.path.isfile(os.path.join(gif_directory, f))]
+    gif_filenames = [f for f in gif_filenames if f.endswith(".gif") and f[0].isdigit()]
+    gif_filenames.sort(key=lambda text: util.parse_leading_int(text, or_else=-1), reverse=True)
+
+    def _key_lookup(key: str):
+        n = util.parse_ending_int(key, or_else=-1)
+        if n < 0 or n >= len(gif_filenames):
+            return None
+        if key.startswith("file_"):
+            return gif_filenames[n]
+        elif key.startswith("name_"):
+            return gif_filenames[n][:-4]  # rm the ".gif" part
+        else:
+            return None
+
+    readme_writer.write_readme("README_template.txt", "README.md",
+                               key_lookup=_key_lookup,
+                               skip_line_if_value_missing=True)
